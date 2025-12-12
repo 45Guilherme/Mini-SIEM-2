701107c13e632e7666d07863466aeeb60cf20c5b
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2025-12-12 01:16:45 -0300

    Update

--- alertas.py
+++ alertas.py
@@ -1,4 +1,30 @@
+from datetime import datetime
+
+def alerta(ip, tentativas, nivel):
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S") 
+    with open("alerta.log", "a", encoding="utf-8") as file:
+        file.write(f"{timestamp} - ATAQUE SUSPEITO do IP: {ip} Tentativas: {tentativas} Cor: {nivel}\n")
+    msg = f"{timestamp} - 🔔 ATAQUE SUSPEITO do IP: {ip} Tentativas: {tentativas} Cor: {nivel}\n"
+    if nivel == "ALTO":         
+       cor = "VERMELHO"
+
+    elif nivel == "MEDIO":
+         
+       cor = "AMARELO"
+    else:
+       cor = "ROXO"
+    print(colorir(msg, cor))
 
 
-def alerta(ip, tentativas):
-    print(f"🚨 ALERTA! Ataque detectado do IP {ip} ({tentativas} tentativas)")
+
+def colorir(texto, cor):
+        cores = {
+        "VERMELHO": "\033[31m",
+        "VERDE": "\033[32m",
+        "AMARELO": "\033[33m",
+        "AZUL": "\033[34m",
+        "CIANO": "\033[36m",
+        "RESET": "\033[0m"
+        }
+        return cores.get(cor, cores["RESET"]) + texto + cores["RESET"]
--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from detector import verificar_ataque
 
 def main():
 
+    tentativas_por_ip = {}
+
+
     log = ["Dec 12 10:02:12 server sshd[1111]: Failed password for root from 123.123.123.1 port 44222\n", 
            "Nov 12 14:34:05 server sshd[1202]: Accepted password for user1 from 192.168.0.10 port 52345 ssh2\n", 
            "Nov 12 14:36:50 server sshd[1205]: Failed password for root from 10.0.0.5 port 50231 ssh2\n"]
     
     for linha in log:
         ip = extrair_info(linha)
+        if ip:
+           tentativas_por_ip[ip] = tentativas_por_ip.get(ip, 0) + 1
+    
 
-    tentativas = 7
-
-    if verificar_ataque(tentativas):
-        alerta(ip, tentativas)
+    for ip, total in tentativas_por_ip.items():
+        if verificar_ataque(total, limite=5):
+            alerta(ip, total)
 
 
 if __name__ == "__main__":
