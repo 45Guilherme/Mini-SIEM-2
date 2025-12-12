dae382bb9ee8263fabda4e429ec7ce212f23703b
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2025-12-12 00:14:18 -0300

    Update

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from parser import extrair_info
 from detector import verificar_ataque
 
 def main():
-    linha = "Nov 12 14:33:01 server sshd[1201]: Failed password for root from 177.233.10.45 port 45821 ssh2"
 
-    ip = extrair_info(linha)
+    log = ["Dec 12 10:02:12 server sshd[1111]: Failed password for root from 123.123.123.1 port 44222\n", 
+           "Nov 12 14:34:05 server sshd[1202]: Accepted password for user1 from 192.168.0.10 port 52345 ssh2\n", 
+           "Nov 12 14:36:50 server sshd[1205]: Failed password for root from 10.0.0.5 port 50231 ssh2\n"]
+    
+    for linha in log:
+        ip = extrair_info(linha)
 
     tentativas = 7
 
--- parser.py
+++ parser.py
@@ -1,11 +1,12 @@
 
 
 def extrair_info(linha):
+        partes = linha.split()
+        for i, parte in enumerate(partes):            
+            if parte == "from" and i + 1 < len(partes):                 
+               return partes[i + 1]
+            
 
-    for linha in linhas:
-        if 
-
-    partes = linha.split()
     
     
 
