59619d215050b25cec1c31d4e80b36ef42c42c59
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2025-12-11 23:02:28 -0300

    Update

---
+++ alertas.py
@@ -0,0 +1,4 @@
+
+
+def alerta(ip, tentativas):
+    print(f"🚨 ALERTA! Ataque detectado do IP {ip} ({tentativas} tentativas)")
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import alertas
+from alertas import alerta
 
 def main():
-    alertas.alerta()
+    ip = "192.168.0.50"   # define o IP antes
+    alerta(ip) 
 
 if __name__ == "__main__":
     main()
