7cd4f7c9c6cefc62037ad791abe59f85bad30996
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2025-12-11 23:37:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from detector import verificar_ataque
 def main():
     linha = "Nov 12 14:33:01 server sshd[1201]: Failed password for root from 177.233.10.45 port 45821 ssh2"
 
+    ip = extrair_info(linha)
+
 
 
 if __name__ == "__main__":
